6b2c3f21be52d8d4933eea6b5c2416d45f2205c7
hadilderbali <115557687+hadilderbali@users.noreply.github.com>
2024-11-13 23:05:46 +0100

    Update profile.service.ts

--- src/app/Services/profile.service.ts
+++ src/app/Services/profile.service.ts
@@ -13,15 +13,15 @@ export class ProfileService {
     console.log('Token:', token); // Log the token
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     console.log('Headers:', headers); // Log the headers
-    return this.http.post<any>('http://localhost:8089/user/api/v1/auth/profile/add', profileData, { headers });
+    return this.http.post<any>('http://192.168.33.10:8089/user/api/v1/auth/profile/add', profileData, { headers });
   }
 
   getDataFromToken(token:string|null){
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     console.log("service",headers)
-    return this.http.post<any>('http://localhost:8089/user/api/v1/auth/decode-token',{},{headers})
+    return this.http.post<any>('http://192.168.33.10:8089/user/api/v1/auth/decode-token',{},{headers})
   }
   getUserById(id:any){
-    return this.http.get<any>(`http://localhost:8089/user/api/v1/auth/user/${id}`)
+    return this.http.get<any>(`http://192.168.33.10:8089/user/api/v1/auth/user/${id}`)
   }
 }
